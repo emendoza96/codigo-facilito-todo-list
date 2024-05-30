443cc2e599bfe67e92314b4ada50eb9c72d61e4e
emendoza96 <emilianomendoza294@gmail.com>
2024-05-30 18:27:47 -0300

    Fix metrics error

--- src/routes/metrics.js
+++ src/routes/metrics.js
@@ -3,9 +3,8 @@ const client = require('prom-client');
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
 
-const metricsMiddleware = (req, res) => {
+module.exports = async (req, res) => {
     res.set('Content-Type', client.register.contentType);
-    res.end(client.register.metrics());
-  };
-
-module.exports = { metricsMiddleware };
+    const metrics = await client.register.metrics();
+    res.end(metrics);
+};
